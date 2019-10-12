d0a09a0be358477626680a34a392dbdeba7e46ce
electrick-1 <elbandido93@mail.ru>
2019-10-12 23:39:45 +0700

    Оптимизация кода

--- js/utils.js
+++ js/utils.js
@@ -1,19 +1,22 @@
 'use strict';
 
 (function () {
+  var generateRandomNumber = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  var generateRandomLength = function (arr) {
+    var array = [];
+    var arrayQuantity = generateRandomNumber(0, arr.length - 1);
+    for (var i = 0; i < arrayQuantity; i++) {
+      array.push(arr[i]);
+    }
+    return array;
+  };
+
   window.utils = {
     KEYCODE_ENTER: 13,
-    generateRandomNumber: function (min, max) {
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    },
-    generateRandomLength: function (arr) {
-      var array = [];
-      var arrayQuantity = this.generateRandomNumber(0, arr.length - 1);
-      for (var i = 0; i < arrayQuantity; i++) {
-        array.push(arr[i]);
-      }
-      return array;
-    },
+    generateRandomNumber: generateRandomNumber,
+    generateRandomLength: generateRandomLength,
     adForm: document.querySelector('.ad-form'),
     roomNumber: document.querySelector('#room_number'),
     capacity: document.querySelector('#capacity')
